02924f45f9370f900d8ebe95dbfa4447f03c8483
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-08-19 14:33:01 -0700

    setup nav and page outlines

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {
+  Text,
+  SafeAreaView,
+  FlatList,
+  Image,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+
+const MainScreen = props => {
+  const navigation = props.navigation;
+  const ListData = [
+    {title: 'Croods', key: 'Details', page: 'Details'},
+    {title: 'Croods2', key: 'Details2', page: 'Details'},
+    {title: 'Croods3', key: 'Details3', page: 'Details'},
+  ];
+
+  const ListItem = ({item}) => (
+    <TouchableOpacity
+      onPress={() => navigation.navigate(item.page, {name: item.key})}>
+      <Image source={require('../imgs/poster.png')} />
+      <Text>{item.title}</Text>
+      <Text>X X X X X</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <SafeAreaView>
+      <Text>Top Kids Picks:</Text>
+      <Button
+        title="searchResults"
+        onPress={() =>
+          navigation.navigate('SearchResults', {name: 'SearchResults'})
+        }>
+        Search Results
+      </Button>
+      <FlatList data={ListData} renderItem={ListItem} />
+    </SafeAreaView>
+  );
+};
+export default MainScreen;
