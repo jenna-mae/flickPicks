20f3b253ae88ac3f0ec8e35fc79e793cf1267b6b
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-08-19 15:48:59 -0700

    added dropdown to home page

--- src/components/PickGenre.js
+++ src/components/PickGenre.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import SelectDropdown from 'react-native-select-dropdown';
 
 const Container = styled.View`
   margin: 40px;
   padding: 20px;
   background-color: #490768;
   border-radius: 20px;
+  text-align: center;
 `;
 
 const Title = styled.Text`
@@ -15,9 +17,22 @@ const Title = styled.Text`
 `;
 
 function PickGenre() {
+  const genres = ['comedy', 'horror', 'action'];
   return (
     <Container>
       <Title>Let's Pick a Genre</Title>
+      <SelectDropdown
+        data={genres}
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          return selectedItem;
+        }}
+        rowTextForSelection={item => {
+          return item;
+        }}
+      />
     </Container>
   );
 }
