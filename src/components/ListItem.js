72bb6cd5e6f5ac5ac60e390ecd411414e6e129a3
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-08-23 21:57:37 -0700

    added hero slider

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
 const ListItem = styled.TouchableOpacity`
-  width: 100%;
-  padding: 14px;
   width: 33.33%;
+  padding: 14px;
 `;
 
 export default ListItem;
