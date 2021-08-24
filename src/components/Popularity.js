597c38b31e5208e4fe6768bf5f73921b9b4c26a2
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-08-23 22:35:56 -0700

    added star functionality

--- src/components/Popularity.js
+++ src/components/Popularity.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Text from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faStar} from '@fortawesome/free-solid-svg-icons';
 
@@ -9,16 +10,60 @@ const Stars = styled.View`
   padding-top: 8px;
 `;
 
-function Popularity() {
-  return (
-    <Stars>
-      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
-      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
-      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
-      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
-      <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
-    </Stars>
-  );
+function Popularity(props) {
+  console.log(props.stars);
+  if (props.stars <= '1000') {
+    return (
+      <Stars>
+        <Text>{props.stars}</Text>
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+      </Stars>
+    );
+  } else if (props.stars <= '2000') {
+    return (
+      <Stars>
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+      </Stars>
+    );
+  } else if (props.stars <= '3000') {
+    return (
+      <Stars>
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+      </Stars>
+    );
+  } else if (props.stars <= '4000') {
+    return (
+      <Stars>
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+      </Stars>
+    );
+  } else {
+    return (
+      <Stars>
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+        <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+      </Stars>
+    );
+  }
 }
 
 export default Popularity;
