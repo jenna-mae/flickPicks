67d3dbb4147ec71c992df057319836bcf94d9718
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-08-23 22:23:14 -0700

    added star icons

---
+++ src/components/Popularity.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faStar} from '@fortawesome/free-solid-svg-icons';
+
+const Stars = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding-top: 8px;
+`;
+
+function Popularity() {
+  return (
+    <Stars>
+      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+      <FontAwesomeIcon icon={faStar} color={'yellow'} size={14} />
+      <FontAwesomeIcon icon={faStar} color={'grey'} size={14} />
+    </Stars>
+  );
+}
+
+export default Popularity;
